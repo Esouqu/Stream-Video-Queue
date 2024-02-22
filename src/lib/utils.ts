f04da73fc815bba3cc77b6143d3aab1319fa5725
esouqu <nippl3juic3@gmail.com>
2024-02-22 22:30:25 +0700

    changed videoId regex

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 export function findYoutubeVideoId(str: string): string | undefined {
   const videoIdRegex =
-    /(?:youtu\.be\/|youtube\.com\/(?:shorts\/|watch\?v=|embed\/|v\/)|youtu\.be\/|\/video\/|watch\?v=|&v=)([a-zA-Z0-9_-]{11})(?:&t=[0-9msh]*)?/;
+    /(?:youtu\.be\/|youtube\.com\/(?:shorts\/|watch\?v=|embed\/|v\/)|youtu\.be\/|\/video\/|watch\?v=|&v=)([a-zA-Z0-9_-]{11})?/;
+  // const videoIdRegex =
+  //   /(?:youtu\.be\/|youtube\.com\/(?:shorts\/|watch\?v=|embed\/|v\/)|youtu\.be\/|\/video\/|watch\?v=|&v=)([a-zA-Z0-9_-]{11})(?:&t=[0-9msh]*)?/;
   // const videoIdRegex =
   // /(?:youtube\.com\/(?:[^/\n\s]+\/(?:shorts?|watch)|(?:v|e(?:mbed)?)\/|\?v=|&v=)|youtu\.be\/|\/video\/|watch\?v=|&v=)([a-zA-Z0-9_-]{11})/;
   const videoIdMatch = str.match(videoIdRegex);
