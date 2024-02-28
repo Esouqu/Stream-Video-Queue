1bf624f4db39c05c02b7039059ffef53c17b4fa8
esouqu <nippl3juic3@gmail.com>
2024-02-29 00:43:12 +0700

    ya ne znay

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,9 @@
 import { DONATIONALERTS_SESSION, TWITCH_SESSION } from "$env/static/private";
-import type { IDonationAlertsUserData, ITwitchUserData } from "$lib/interfaces";
+import type { IDonationAlertsRefreshToken, IDonationAlertsUserData, ITwitchUserData } from "$lib/interfaces";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ cookies, fetch }) => {
+  let donationalertsSession = cookies.get(DONATIONALERTS_SESSION);
   let twitchChannel: ITwitchUserData | undefined;
   let donationAlertsUser: IDonationAlertsUserData | undefined;
 
@@ -19,9 +20,16 @@ export const load: LayoutServerLoad = async ({ cookies, fetch }) => {
       .then((data: ITwitchUserData) => data);
   }
 
-  await fetch('/api/donationalerts/refresh', { method: 'POST' })
+  if (!donationalertsSession) {
+    const refreshTokenResponse = await fetch('/api/donationalerts/refresh', { method: 'POST' })
+      .then((res) => res);
 
-  if (cookies.get(DONATIONALERTS_SESSION)) {
+    if (refreshTokenResponse.status === 200) {
+      donationalertsSession = await refreshTokenResponse.json().then((data: IDonationAlertsRefreshToken) => data.access_token);
+    }
+  }
+
+  if (donationalertsSession) {
     donationAlertsUser = await fetch('/api/donationalerts/user')
       .then((res) => res.json())
       .then((data: IDonationAlertsUserData) => data);
