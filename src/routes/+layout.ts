c36b5bc7c4aa84da5f6ed4167a6fe47f7b5c746e
esouqu <nippl3juic3@gmail.com>
2024-03-03 04:53:58 +0700

    testing

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,5 +2,7 @@ import { dev } from '$app/environment';
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 import { inject } from '@vercel/analytics';
 
+export const ssr = false;
+
 inject({ mode: dev ? 'development' : 'production' });
 if (!dev) injectSpeedInsights();
