71567ccd3c4a6bec3b7da4fb5df8dc51f161b33f
esouqu <nippl3juic3@gmail.com>
2024-03-03 05:52:53 +0700

    testing

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,13 +2,12 @@
 // import { TWITCH_REFRESH_TOKEN, TWITCH_SESSION } from "$env/static/private";
 import type { HandleFetch } from "@sveltejs/kit";
 
-export const handleFetch: HandleFetch = async ({ request, fetch }) => {
-  console.log(request.url)
-  // const session = event.request.headers.get('cookie');
+export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
+  const cookies = event.request.headers.get('cookie');
 
-  // if (request.url.startsWith('https://api.my-domain.com/') && session) {
-  //   request.headers.set('cookie', session);
-  // }
-  // console.log(event.request.headers.get('cookie'));
+  if (request.url.startsWith('https://stream-video-queue.vercel.app/') && cookies) {
+    request.headers.set('cookie', cookies);
+  }
+
   return fetch(request);
 }
