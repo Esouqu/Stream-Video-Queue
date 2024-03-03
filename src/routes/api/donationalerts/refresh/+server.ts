97bf7c09ed066743c788bb1f992e4ffc01b86d13
esouqu <nippl3juic3@gmail.com>
2024-03-03 15:25:45 +0700

    testing

--- src/routes/api/donationalerts/refresh/+server.ts
+++ src/routes/api/donationalerts/refresh/+server.ts
@@ -23,11 +23,10 @@ export const POST: RequestHandler = async ({ cookies }) => {
       })
     }).then((res) => res);
 
-    if (response.status === 401) {
-      return new Response('The donation alerts refresh token is invalid', { status: 401 });
-    }
+    console.log(response)
 
     const tokenData = await response.json().then((data: IDonationAlertsRefreshToken) => data);
+    console.log(tokenData)
 
     cookies.set(DONATIONALERTS_SESSION, tokenData.access_token, {
       path: '/',
