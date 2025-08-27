2fd00078d97e77a34fbd18092f33b67cadd6f207
Esouqu <nippl3juic3@gmail.com>
2025-08-27 16:01:14 +0700

    deps update

--- src/lib/components/icons/GithubIcon.svelte
+++ src/lib/components/icons/GithubIcon.svelte
@@ -7,7 +7,7 @@
 	const { size = '1.5rem', color = 'currentColor' }: Props = $props();
 </script>
 
-<svg width={size} height={size} xmlns="http://www.w3.org/2000/svg">
+<svg width={size} height={size} viewBox="0 0 97.707 97.707" xmlns="http://www.w3.org/2000/svg">
 	<path
 		fill-rule="evenodd"
 		clip-rule="evenodd"
